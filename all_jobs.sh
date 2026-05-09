04771030f9523954044059d1426ed249f3b84bbe
RongqinChen <yc07917@umac.mo>
2026-05-10 00:36:06 +0800

    update

---
+++ all_jobs.sh
@@ -0,0 +1,13 @@
+
+python train.py experiment=qm9.yaml label=mu
+python train.py experiment=qm9.yaml label=alpha
+python train.py experiment=qm9.yaml label=homo
+python train.py experiment=qm9.yaml label=lumo
+python train.py experiment=qm9.yaml label=gap
+python train.py experiment=qm9.yaml label=r2
+python train.py experiment=qm9.yaml label=zpve
+python train.py experiment=qm9.yaml label=U0
+python train.py experiment=qm9.yaml label=U
+python train.py experiment=qm9.yaml label=H
+python train.py experiment=qm9.yaml label=G
+python train.py experiment=qm9.yaml label=Cv
