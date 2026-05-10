006ddcf2bb2a319f7af82d561fd12671df1f3545
RongqinChen <yc07917@umac.mo>
2026-05-11 00:07:06 +0800

    update

--- all_jobs.sh
+++ all_jobs.sh
@@ -1,13 +1,15 @@
+export CUDA_VISIBLE_DEVICES=0
 
-python train.py experiment=qm9.yaml label=mu
-python train.py experiment=qm9.yaml label=alpha
-python train.py experiment=qm9.yaml label=homo
-python train.py experiment=qm9.yaml label=lumo
-python train.py experiment=qm9.yaml label=gap
-python train.py experiment=qm9.yaml label=r2
-python train.py experiment=qm9.yaml label=zpve
-python train.py experiment=qm9.yaml label=U0
-python train.py experiment=qm9.yaml label=U
+# python train.py experiment=qm9.yaml label=mu
+# python train.py experiment=qm9.yaml label=alpha
+# python train.py experiment=qm9.yaml label=homo
+# python train.py experiment=qm9.yaml label=lumo
+# python train.py experiment=qm9.yaml label=gap
+# python train.py experiment=qm9.yaml label=r2
+
+# python train.py experiment=qm9.yaml label=Cv
+# python train.py experiment=qm9.yaml label=G
 python train.py experiment=qm9.yaml label=H
-python train.py experiment=qm9.yaml label=G
-python train.py experiment=qm9.yaml label=Cv
+# python train.py experiment=qm9.yaml label=U
+# python train.py experiment=qm9.yaml label=U0
+# python train.py experiment=qm9.yaml label=zpve
