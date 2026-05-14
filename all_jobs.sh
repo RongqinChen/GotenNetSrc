fcebef646761166c7281910f1a08400c7132429d
RongqinChen <yc07917@umac.mo>
2026-05-14 13:25:56 +0800

    update

--- all_jobs.sh
+++ all_jobs.sh
@@ -19,11 +19,14 @@ python train.py experiment=qm9.yaml label=zpve  logger=csv.yaml
 python train.py experiment=rmd17.yaml label=aspirin         logger=csv.yaml
 python train.py experiment=rmd17.yaml label=azobenzene      logger=csv.yaml
 python train.py experiment=rmd17.yaml label=benzene         logger=csv.yaml
+
 python train.py experiment=rmd17.yaml label=ethanol         logger=csv.yaml
 python train.py experiment=rmd17.yaml label=malonaldehyde   logger=csv.yaml
 python train.py experiment=rmd17.yaml label=naphthalene     logger=csv.yaml
+
 python train.py experiment=rmd17.yaml label=paracetamol     logger=csv.yaml
 python train.py experiment=rmd17.yaml label=salicylic       logger=csv.yaml
+
 python train.py experiment=rmd17.yaml label=toluene         logger=csv.yaml
 python train.py experiment=rmd17.yaml label=uracil          logger=csv.yaml
 
